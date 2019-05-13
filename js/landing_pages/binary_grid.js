87b241bb87c6eff050bac48b3bb6cba5fbc44981
OskarAhl <oskar@binary.com>
2019-05-13 15:09:27 +0200

    Release to staging

--- js/landing_pages/binary_grid.js
+++ js/landing_pages/binary_grid.js
@@ -1,2 +1,2 @@
-window.onload=function(){commonOnload()};
+fetch('https://grid.binary.me/version.json').then(function(response){return response.json()}).then(function(app){$('.download').attr('href','https://grid.binary.me/download/'+app.name)});window.onload=function(){commonOnload()};
 //# sourceMappingURL=binary_grid.js.map
