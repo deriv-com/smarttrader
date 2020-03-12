06397549158e450090d16102a9a2017d474ba97b
Aaron Imming <aaim@protonmail.com>
2020-03-12 13:43:52 +0800

    Make "Ready to trade" large btn

--- src/templates/app/trade/trading.jsx
+++ src/templates/app/trade/trading.jsx
@@ -208,7 +208,7 @@ const Trading = () => (
                             <div id='authorization_error_container' className='center-text invisible'>
                                 <div className='authorization_error'>
                                     <h3 id='authorization_error_text'>{it.L('Ready to trade?')}</h3>
-                                    <a className='btn btn--primary' href='https://www.deriv.com/signup/'>
+                                    <a className='btn btn__large btn--primary' href='https://www.deriv.com/signup/'>
                                         <span>{it.L('Open a free account')}</span>
                                     </a>
                                     <p>{it.L('Already have an account?')}</p>
