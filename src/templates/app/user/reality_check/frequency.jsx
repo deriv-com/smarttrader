bcf1a29fcd87c0d34b71706dd42da5e2ddff69e6
suthesan <suthesankanagasabai@gmail.com>
2020-07-28 17:54:41 +0800

    redesigned reality check screen

--- src/templates/app/user/reality_check/frequency.jsx
+++ src/templates/app/user/reality_check/frequency.jsx
@@ -4,7 +4,8 @@ import Wrapper from './wrapper.jsx';
 const Frequency = () => (
     <React.Fragment>
         <Wrapper>
-            <p>{it.L('Options trading can become a real addiction, as can any other activity pushed to its limits. To avoid the danger of such an addiction, we provide a reality-check that gives you a summary of your trades and accounts on a regular basis.')}</p>
+            <div className = 'reality-check__label'>{it.L('Options trading can become a real addiction, as can any other activity pushed to its limits. To avoid the danger of such an addiction, we provide a reality-check that gives you a summary of your trades and accounts on a regular basis.')}</div>
+            <div className = 'reality-check__label'>{it.L('Would you like to check your statement first? [_1]Check Statement[_2]', '<a href="javascript:;" id="statement" className = "reality-check__label reality-check__label-link">', '</a>')}</div>
         </Wrapper>
     </React.Fragment>
 );
