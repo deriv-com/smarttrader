357b7d314296600b1642762ca81221719262fb10
Amir Ghadimi <amir.ghadimi@firstsource.tech>
2021-06-13 17:12:03 +0430

    fix: remove binary-static from js url on dev mode

--- build/webpack/helpers.js
+++ build/webpack/helpers.js
@@ -12,8 +12,7 @@ const makeCacheGroup = (name, priority, ...matches) => ({
 });
 
 const publicPathFactory = (grunt) => () => (
-    (global.is_release || grunt.file.exists(PATHS.ROOT, 'scripts/CNAME') ? '' : '/binary-static') +
-    (global.branch ? `/${global.branch_prefix}${global.branch}` : '') +
+    (global.branch ? `/binary-static/${global.branch_prefix}${global.branch}` : '') +
     '/js/'
 );
 
