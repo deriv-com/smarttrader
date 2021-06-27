cf953edf3194a49dd85308040308048eb431de51
Mahdi Aryayi <55829032+mahdiaryayi-fs@users.noreply.github.com>
2021-06-27 15:04:23 +0430

    Mahdi / Hotfix on API token form (Reverted #6595) (#6634)
    * Revert "Nikita/Add funds redirect button in MT5 manage funds not redirecting to cashier page in one click. (#6595)"
    
    This reverts commit abe860108fc0ac24323c3fae3e7c693c0c48768c.
    
    * fixed validation bug
    
    * Fixed linter bug

--- src/javascript/app/common/form_validation.js
+++ src/javascript/app/common/form_validation.js
@@ -101,10 +101,9 @@ const Validation = (() => {
                     const event = events_map[field.type];
 
                     if (event) {
-                        field.$.unbind(event).on(event, (e) => {
-                            if (e.type === 'input'){
-                                checkField(field);
-                            }
+                        field.$.unbind(event).on(event, e => {
+                            if (e.type === 'change' && e.target.type === 'text') return;
+                            checkField(field);
                             if (field.re_check_field) {
                                 checkField(forms[form_selector].fields.find(fld => (
                                     fld.selector === field.re_check_field
