b916d5a03381991e5fd6c1db04a19e2b3eaaa9b9
Akmal Djumakhodjaev <akmal@binary.com>
2021-08-30 16:48:08 +0800

    Make state mandatory for australians (#6743)

--- src/javascript/app/common/account_opening.js
+++ src/javascript/app/common/account_opening.js
@@ -276,6 +276,7 @@ const AccountOpening = (() => {
     };
 
     const commonValidations = () => {
+        const residence = Client.get('residence');
         const req = [
             { selector: '#salutation',                  validations: ['req'] },
             { selector: '#first_name',                  validations: ['req', 'letter_symbol', ['length', { min: 2, max: 50 }]] },
@@ -284,8 +285,8 @@ const AccountOpening = (() => {
             { selector: '#address_line_1',              validations: ['req', 'address', ['length', { min: 1, max: 70 }]] },
             { selector: '#address_line_2',              validations: ['address', ['length', { min: 0, max: 70 }]] },
             { selector: '#address_city',                validations: ['req', 'letter_symbol', ['length', { min: 1, max: 35 }]] },
-            { selector: '#address_state',               validations: $('#address_state').prop('nodeName') === 'SELECT' ? '' : ['letter_symbol', ['length', { min: 0, max: 35 }]] },
-            { selector: '#address_postcode',            validations: [Client.get('residence') === 'gb' || State.getResponse('authorize.upgradeable_landing_companies').some(lc => lc === 'iom') ? 'req' : '', 'postcode', ['length', { min: 0, max: 20 }]] },
+            { selector: '#address_state',               validations: [residence === 'au' ? 'req' : '', $('#address_state').prop('nodeName') === 'SELECT' ? '' : ['letter_symbol', ['length', { min: 0, max: 35 }]]] },
+            { selector: '#address_postcode',            validations: [residence === 'gb' || State.getResponse('authorize.upgradeable_landing_companies').some(lc => lc === 'iom') ? 'req' : '', 'postcode', ['length', { min: 0, max: 20 }]] },
             { selector: '#phone',                       validations: ['req', 'phone', ['length', { min: 9, max: 35, value: () => $('#phone').val().replace(/\D/g,'') }]] },
             { selector: '#secret_question',             validations: ['req'] },
             { selector: '#secret_answer',               validations: ['req', 'general', ['length', { min: 4, max: 50 }]] },
