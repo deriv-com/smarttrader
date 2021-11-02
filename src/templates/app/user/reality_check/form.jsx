806858e70d42a02b9ffaf2fe2e35def0d7369917
pavel-latyshou-deriv <90393787+pavel-latyshou-deriv@users.noreply.github.com>
2021-11-02 10:15:36 +0300

    fix: add margin to viev statement button (#6903)
    Co-authored-by: “pavel-latyshou-deriv” <“pavel.latyshou@regentmarkets.com”>

--- src/templates/app/user/reality_check/form.jsx
+++ src/templates/app/user/reality_check/form.jsx
@@ -10,7 +10,7 @@ const Form = () => (
             </div>
             <div className='center-text gr-centered gr-padding-20 gr-child' id='reality_check_nav'>
                 <button className='button' type='submit'>{it.L('Continue trading')}</button>
-                <a className='button button-secondary' id='statement' href='javascript:;'><span>{it.L('View statement')}</span></a>
+                <a className='button button-secondary' id='statement' href='javascript:;'><span className='button-secondary'>{it.L('View statement')}</span></a>
                 <a className='button button-secondary' id='logout' href='javascript:;'><span>{it.L('Log out')}</span></a>
             </div>
         </form>
