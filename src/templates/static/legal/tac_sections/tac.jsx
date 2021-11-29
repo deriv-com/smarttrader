4714dd60942d51c2c029ee7eb11eb101ea49896c
pavel-latyshou-deriv <90393787+pavel-latyshou-deriv@users.noreply.github.com>
2021-11-29 10:02:52 +0300

    change tnc ersion (#6992)
    Co-authored-by: “pavel-latyshou-deriv” <“pavel.latyshou@regentmarkets.com”>

--- src/templates/static/legal/tac_sections/tac.jsx
+++ src/templates/static/legal/tac_sections/tac.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Tac = () => (
     <div>
         <h2 data-anchor='terms-and-conditions'>{it.L('Terms and conditions')}</h2>
-        <p><i>{it.L('Version 51. Last modified 17-11-2021.')}</i></p>
+        <p><i>{it.L('Version 52. Last modified 26-11-2021.')}</i></p>
         <p>{it.L('It is the responsibility of each client to read and understand this legal notice and the terms and conditions ("Terms") pursuant to which an acquisition of financial contracts via this site is governed.')}</p>
 
         <h2 data-anchor='interpretation'>A. {it.L('Interpretation')}</h2>
