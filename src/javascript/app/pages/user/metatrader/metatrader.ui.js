ebfeb5e13e60c19cefe281f964054492f24b7cab
hirad-rewok <91878582+hirad-rewok@users.noreply.github.com>
2022-03-15 06:42:25 +0330

    fix--On MT5 server suspend, All of account creations are disabled (#7023)
    Co-authored-by: Kevin <70131533+kevinw-binary@users.noreply.github.com>

--- src/javascript/app/pages/user/metatrader/metatrader.ui.js
+++ src/javascript/app/pages/user/metatrader/metatrader.ui.js
@@ -963,9 +963,11 @@ const MetaTraderUI = (() => {
 
         // disable next button and Synthetic option if all servers are used or unavailable
         const num_servers = populateTradingServers('real_gaming_financial');
-        if (/real/.test(selected_acc_type) && num_servers.supported === num_servers.used + num_servers.disabled) {
+        if (/(real|demo)/.test(selected_acc_type) && num_servers.supported === num_servers.used + num_servers.disabled) {
             disableButtonLink('.btn-next');
             $form.find('.step-2 #rbtn_gaming_financial').addClass('existed');
+            $form.find('.step-2 #rbtn_financial_financial').addClass('existed');
+            $form.find('.step-2 #rbtn_financial_financial_stp').addClass('existed');
         }
     };
 
