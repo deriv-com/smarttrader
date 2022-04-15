3a077302f81cc7f26de240eb71ea86951e75a0ff
Matin-binary <matin@deriv.com>
2022-04-15 14:26:56 +0800

    Chore: Running NPM audit, cleaning and updating packages

--- build/webpack.js
+++ build/webpack.js
@@ -1,11 +1,6 @@
-const path           = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const webpack        = require('webpack');
 const webpackMerge   = require('webpack-merge');
 const appConfig      = require('./webpack/config_app');
 const commonConfig   = require('./webpack/config_common');
-const PATHS          = require('./webpack/paths');
-const getPlugins     = require('./webpack/plugins');
 
 module.exports = function (grunt) {
     const section = [webpackMerge.smart(commonConfig(grunt), appConfig(grunt))];
