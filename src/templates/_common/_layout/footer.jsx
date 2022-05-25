d9419ea3187143882c6d8a2d4226eb5d2a254a4b
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2022-05-25 13:19:35 +0800

    add file rename (#7111)

--- src/templates/_common/_layout/footer.jsx
+++ src/templates/_common/_layout/footer.jsx
@@ -178,7 +178,7 @@ const Footer = () => (
                                     </a>
                                 </div>
                                 <div className='age-restriction-sign'>
-                                    <img className='responsive' src={it.url_for('images/pages/footer/18+.svg')} />
+                                    <img className='responsive' src={it.url_for('images/pages/footer/18.svg')} />
                                 </div>
                             </div>
                             <SocialIcons
