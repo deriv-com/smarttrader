683131b99dc266e26bfc7e7c740b5f2ab366037d
maryia-deriv <maryia@binary.com>
2024-03-19 17:51:57 +0300

    build: trigger

--- build/checkDependencies.js
+++ build/checkDependencies.js
@@ -1,7 +1,7 @@
 module.exports = {
     all: {
         options: {
-            install  : false,
+            install  : true,
             verbose  : true,
             scopeList: ['dependencies', 'devDependencies'],
         },
