9c9ead2b3a8f7fee290efb2cdc14d91ef03e8917
prince-deriv <prince@deriv.com>
2024-06-04 16:06:30 +0400

    chore: update webpack config to load css from quill library

--- build/webpack/config_common.js
+++ build/webpack/config_common.js
@@ -62,6 +62,11 @@ const commonConfig = (grunt) => ({
                     },
                 ],
             },
+            {
+                test: /\.css$/,
+                use : ['style-loader', 'css-loader'],
+            },
+               
         ],
     },
     watch       : false,
