16d3d421e27e6e3a5cd8a1dad846a5e2a8f7e1b6
prince-deriv <prince@deriv.com>
2024-07-22 13:11:17 +0400

    fix: dropdown not being selected properly on digit info

--- src/javascript/app/pages/trade/markets/markets-dropdown.jsx
+++ src/javascript/app/pages/trade/markets/markets-dropdown.jsx
@@ -198,12 +198,14 @@ export const MarketsDropdown = () => {
         setSelectedMarket(underlying);
         triggerMarketChange();
 
-        // Old Trigger
-        const underlyingElement = getElementById('underlying');
-        const event = new Event('change');
-        underlyingElement.value = underlying;
-        underlyingElement.setAttribute('data-text', underlyings[underlying]);
-        underlyingElement.dispatchEvent(event);
+        // const underlyingElement = getElementById('underlying');
+        // const event = new Event('change');
+        // underlyingElement.value = underlying;
+        // underlyingElement.setAttribute('data-text', underlyings[underlying]);
+        // underlyingElement.dispatchEvent(event);
+
+        // Trigger the old form to enable other required effects from it
+        document.querySelectorAll('.symbol_name').forEach(el => el.id === underlying && el.click());
 
         closeMarketDropdown();
     };
