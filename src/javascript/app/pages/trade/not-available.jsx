cef0a6dfca9ba51ee1b59e926dd42d4d64f03755
jia-deriv <jiawei@regentmarkets.com>
2024-07-24 16:46:35 +0800

    chore: add button

--- src/javascript/app/pages/trade/not-available.jsx
+++ src/javascript/app/pages/trade/not-available.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Text } from '@deriv-com/quill-ui';
+import { Text, Button } from '@deriv-com/quill-ui';
 import { getElementById } from '../../../_common/common_functions';
 import { localize } from '../../../_common/localize.js';
 
 const NotAvailable = () => (
-    <div className='container'>
-        <section className='not-available-wrapper'>
-            <Text size='xl' bold centered className='text-width'>
+    <div className='not-available-container'>
+        <section className='not-available-section'>
+            <Text size='xl' bold centered>
                 {localize('SmartTrader is unavailable for this account')}
             </Text>
-            <Text size='lg' centered className='text-width'>
+            <Text size='lg' centered>
                 {localize(
                     'Unfortunately, this trading platform is not available for EU Deriv account. Please switch to a non-EU account to continue trading.'
                 )}
             </Text>
+            <Button onClick={() => getElementById('acc_switcher').click()} size='lg'>Switch to another account</Button>
         </section>
     </div>
 );
