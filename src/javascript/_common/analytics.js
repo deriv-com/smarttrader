2a4cb2872b35265dae77f2f13a3feab77a6f8422
Adrienne Rio <adrienne@deriv.com>
2024-10-02 12:00:54 +0800

    chore: removed growthbook decryption key as it is not required

---
+++ src/javascript/_common/analytics.js
@@ -0,0 +1,42 @@
+const DerivAnalytics = require('@deriv-com/analytics');
+
+const Analytics = (() => {
+    const init = () => {
+        if (process.env.RUDDERSTACK_KEY && process.env.GROWTHBOOK_CLIENT_KEY) {
+            DerivAnalytics.Analytics.initialise({
+                growthbookKey          : process.env.GROWTHBOOK_CLIENT_KEY, // optional key to enable A/B tests
+                rudderstackKey         : process.env.RUDDERSTACK_KEY,
+            });
+        }
+    };
+
+    const isGrowthbookLoaded = () => Boolean(DerivAnalytics.Analytics?.getInstances()?.ab);
+
+    const getGrowthbookFeatureValue = ({ defaultValue, featureFlag }) => {
+        const resolvedDefaultValue = defaultValue !== undefined ? defaultValue : false;
+        const isGBLoaded = isGrowthbookLoaded();
+
+        if (!isGBLoaded) return [null, false];
+
+        return [DerivAnalytics.Analytics?.getFeatureValue(featureFlag, resolvedDefaultValue), true];
+    };
+
+    const setGrowthbookOnChange = onChange => {
+        const isGBLoaded = isGrowthbookLoaded();
+        if (!isGBLoaded) return null;
+
+        const onChangeRenderer = DerivAnalytics.Analytics?.getInstances().ab.GrowthBook?.setRenderer(() => {
+            onChange();
+        });
+        return onChangeRenderer;
+    };
+
+    return {
+        init,
+        isGrowthbookLoaded,
+        getGrowthbookFeatureValue,
+        setGrowthbookOnChange,
+    };
+})();
+
+module.exports = Analytics;
