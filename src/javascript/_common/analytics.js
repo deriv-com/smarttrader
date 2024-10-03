0deec9e172106c3b5adb870abb0fdd8afe6f523c
Adrienne Rio <adrienne@deriv.com>
2024-10-03 14:24:55 +0800

    chore: removed decryption key

--- src/javascript/_common/analytics.js
+++ src/javascript/_common/analytics.js
@@ -2,10 +2,9 @@ const DerivAnalytics = require('@deriv-com/analytics');
 
 const Analytics = (() => {
     const init = () => {
-        if (process.env.RUDDERSTACK_KEY && process.env.GROWTHBOOK_CLIENT_KEY && process.env.GROWTHBOOK_DECRYPTION_KEY) {
+        if (process.env.RUDDERSTACK_KEY && process.env.GROWTHBOOK_CLIENT_KEY) {
             DerivAnalytics.Analytics.initialise({
                 growthbookKey          : process.env.GROWTHBOOK_CLIENT_KEY, // optional key to enable A/B tests
-                growthbookDecryptionKey: process.env.GROWTHBOOK_DECRYPTION_KEY, // optional key to enable A/B tests
                 rudderstackKey         : process.env.RUDDERSTACK_KEY,
             });
         }
