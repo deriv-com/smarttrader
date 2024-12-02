16e3a28085c71e985b23b1cde1e3dcd393abdfc9
Adrienne Rio <adrienne@deriv.com>
2024-12-02 17:08:58 +0800

    chore: debug cookie

--- src/javascript/app/pages/callback/callback.jsx
+++ src/javascript/app/pages/callback/callback.jsx
@@ -116,17 +116,14 @@ const CallbackContainer = () => {
                 }
             }
             getElementById('loading_link').setAttribute('href', redirect_url);
-
-            const domains = ['deriv.com', 'binary.sx', 'pages.dev', 'localhost'];
-            const currentDomain = window.location.hostname.split('.').slice(-2).join('.');
-            if (domains.includes(currentDomain)) {
-                Cookies.set('logged_state', 'true', {
-                    expires: 30,
-                    path   : '/',
-                    domain : currentDomain,
-                    secure : true,
-                });
-            }
+            
+            // eslint-disable-next-line
+            console.log('setting cookie....SHOULD BE TRUEEEE')
+            Cookies.set('logged_state', 'true', {
+                expires: 30,
+                path   : '/',
+                secure : true,
+            });
 
             window.location.href = redirect_url; // need to redirect not using pjax
         });
