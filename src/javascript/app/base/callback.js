0a3270d2b61bd22a60132345babd27dd185df667
Adrienne Rio <adrienne@deriv.com>
2024-12-02 16:49:03 +0800

    chore: debug

--- src/javascript/app/base/callback.js
+++ src/javascript/app/base/callback.js
@@ -5,7 +5,7 @@ const CallbackHandler = (() => {
     const onLoad = () => {
         SocketCache.clear();
         CallbackElement.init();
-        parent.window.is_logging_in = 1; // this flag is used in base.js to prevent auto-reloading this page
+        parent.window.is_logging_in = 1; // this flag is used in base.js to prevent auto-reloading this pag
     };
 
     return {
