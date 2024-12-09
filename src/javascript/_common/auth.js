f012acf08260d6ccaa8cf923eb69f7d6f80fb70f
Adrienne Rio <adrienne@deriv.com>
2024-12-09 17:28:21 +0800

    chore: debug

--- src/javascript/_common/auth.js
+++ src/javascript/_common/auth.js
@@ -92,9 +92,10 @@ export const requestSingleLogout = async (onWSLogoutAndRedirect) => {
         const isCallbackPage = window.location.pathname.includes('callback');
         const isEndpointPage = window.location.pathname.includes('endpoint');
 
-        const logoutHandler = getLogoutHandler(onWSLogoutAndRedirect);
-
+        // eslint-disable-next-line
+        console.log('are we logging out', isLoggedOutCookie, isClientAccountsPopulated, isAuthEnabled, isCallbackPage, isEndpointPage);
         if (isLoggedOutCookie && isClientAccountsPopulated && isAuthEnabled && !isCallbackPage && !isEndpointPage) {
+            const logoutHandler = getLogoutHandler(onWSLogoutAndRedirect);
             await logoutHandler(onWSLogoutAndRedirect);
         }
     };
