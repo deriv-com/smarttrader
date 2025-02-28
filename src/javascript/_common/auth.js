0a911a84cc2c0051e050cb30a03d1cd9d3c690f9
vinu-deriv <vinu@regentmarkets.com>
2025-02-28 14:02:16 +0400

    fix: remove calling of oidc when token is missing

--- src/javascript/_common/auth.js
+++ src/javascript/_common/auth.js
@@ -143,20 +143,11 @@ export const requestSingleSignOn = async () => {
         // if client.accounts in localStorage is empty - !isClientAccountsPopulated
         // and if feature flag for OIDC Phase 2 is enabled - isAuthEnabled
         // Check if any account or its linked account is missing a token
-
-        const hasMissingToken = Object.values(clientAccounts).some((account) => {
-            // Check if current account is missing token
-            if (!account?.token && account?.is_disabled !== 1) {
-                return true; // No linked accounts and no token
-            }
-            return false;
-        });
-
         const shouldRequestSignOn =
           isLoggedInCookie &&
           !isCallbackPage &&
           !isEndpointPage &&
-          (!isClientAccountsPopulated || hasMissingToken) &&
+          (!isClientAccountsPopulated) &&
           isAuthEnabled;
 
         if (shouldRequestSignOn) {
