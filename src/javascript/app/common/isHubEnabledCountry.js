252f64e7516ab2a5b7a10bbe07c051db77b16181
vinu-deriv <100689171+vinu-deriv@users.noreply.github.com>
2025-03-18 19:43:59 +0800

    Update isHubEnabledCountry.js
    added console.log for debugging purpose only

--- src/javascript/app/common/isHubEnabledCountry.js
+++ src/javascript/app/common/isHubEnabledCountry.js
@@ -9,6 +9,9 @@ import { getTopLevelDomain } from '../../_common/utility';
  */
 const isHubEnabledCountry = () => {
     const is_com_domain = getTopLevelDomain() === 'com';
+
+    console.log('is_com_domain', is_com_domain);
+    console.log('ClientBase.hasWalletsAccount()', ClientBase.hasWalletsAccount());
     
     if (!is_com_domain || !ClientBase.hasWalletsAccount()) {
         return false;
