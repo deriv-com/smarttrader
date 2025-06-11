75812e0d0dbd6b93219777ff4a900a14742d9295
Akmal Djumakhodjaev <akmal@binary.com>
2025-06-11 14:23:07 +0800

    refactor: remove unused theme prop from Loading component

--- src/templates/_common/components/loading.jsx
+++ src/templates/_common/components/loading.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Loading = ({ is_invisible, theme, id }) => (
+const Loading = ({ is_invisible, id }) => (
     <div id={id} className={`initial-loader__barspinner barspinner barspinner-light${is_invisible ? ' invisible' : ''}`}>
-        <div className="initial-loader__barspinner--rect barspinner__rect barspinner__rect--1 rect1" />
-        <div className="initial-loader__barspinner--rect barspinner__rect barspinner__rect--2 rect2" />
-        <div className="initial-loader__barspinner--rect barspinner__rect barspinner__rect--3 rect3" />
-        <div className="initial-loader__barspinner--rect barspinner__rect barspinner__rect--4 rect4" />
-        <div className="initial-loader__barspinner--rect barspinner__rect barspinner__rect--5 rect5" />
+        <div className='initial-loader__barspinner--rect barspinner__rect barspinner__rect--1 rect1' />
+        <div className='initial-loader__barspinner--rect barspinner__rect barspinner__rect--2 rect2' />
+        <div className='initial-loader__barspinner--rect barspinner__rect barspinner__rect--3 rect3' />
+        <div className='initial-loader__barspinner--rect barspinner__rect barspinner__rect--4 rect4' />
+        <div className='initial-loader__barspinner--rect barspinner__rect barspinner__rect--5 rect5' />
     </div>
 );
 
