b00176bdab1d577a1c48ccc99d7eb92e39b16bca
Akmal Djumakhodjaev <akmal@binary.com>
2025-07-14 13:46:27 +0800

    fix: enhance domain handling in getAllowedLocalStorageOrigin function

--- src/javascript/_common/url.js
+++ src/javascript/_common/url.js
@@ -128,18 +128,20 @@ const Url = (() => {
 
     const getAllowedLocalStorageOrigin = (is_traders_hub_or_wallet) => {
         // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+        const domain_zone = getTopLevelDomain();
+        
         if (
             /^smarttrader-staging\.deriv\.app$/i.test(window.location.hostname) ||
             /^staging-smarttrader\.deriv\.com$/i.test(window.location.hostname)
         ) {
-            return is_traders_hub_or_wallet ? 'https://staging-hub.deriv.com' : 'https://staging-app.deriv.com';
+            return is_traders_hub_or_wallet ? `https://staging-hub.deriv.com` : 'https://staging-app.deriv.com';
         } else if (
             /^smarttrader\.deriv\.app$/i.test(window.location.hostname) ||
             /^smarttrader\.deriv\.com$/i.test(window.location.hostname)
         ) {
-            return is_traders_hub_or_wallet ? 'https://hub.deriv.com' : deriv_app_domain;
+            return is_traders_hub_or_wallet ? `https://hub.deriv.${domain_zone}` : deriv_app_domain;
         }
-        return is_traders_hub_or_wallet ? 'https://hub.deriv.com' : deriv_app_domain;
+        return is_traders_hub_or_wallet ? `https://hub.deriv.${domain_zone}` : deriv_app_domain;
     };
 
     /**
