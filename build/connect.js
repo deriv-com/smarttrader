e3b1e3681de8e1c286ea2ab54f5d39906594d81c
farabi-deriv <farabi@regentmarkets.com>
2025-07-15 16:31:02 +0800

    fix: revert port changes

--- build/connect.js
+++ build/connect.js
@@ -10,14 +10,14 @@ module.exports = function (grunt) {
         livereload: {
             options: {
                 hostname   : '0.0.0.0',
-                port       : 8443,
+                port       : 8090,
                 protocol   : 'https',
                 base       : 'dist',
                 open       : {
                     appName: {
                         name: 'Google\ Chrome'
                     },
-                    target : 'https://localhost:8443',
+                    target : 'https://localhost:8090',
                 },
                 middleware: (connect, options) => {
                     const middlewares = [
